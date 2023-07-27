849b317c3bb0e8accc29c20b1d177f8963b79dbd
mxrgxr <mgxrcias@gmail.com>
2023-07-26 22:14:45 -0700

    create plant model, implement CRUD functionality for plants using class-based views

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Plant(models.Model):
+    common_name = models.CharField(max_length=50)
+    genus = models.CharField(max_length=20)
+    species = models.CharField(max_length=20)
+    plant_type = models.CharField(max_length=50)
+    country_of_origin = models.CharField(max_length=20)
+    endangered = models.BooleanField()
+    def __str__(self):
+        return f'{self.common_name} ({self.id})'
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'plant_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,32 +1,29 @@
-plants = [
-    {
-        "name": "Maxillaria sophronitis",
-        "genus_species": "Maxillaria sophronitis (Rchb.f.) Garay",
-        "conservation_status": "Least Concern",
-        "plant_type": "Pseudobulbous epiphyte",
-        "country_of_origin": "Colombia to Venezuela",
-    },
-    {
-        "name": "Philodendron lazorii",
-        "genus_species": "Philodendron lazorii Croat",
-        "conservation_status": "Least Concern",
-        "plant_type": "Climber",
-        "country_of_origin": "Panama to NW. Colombia",
-    },
-    {
-        "name": "Espeletia funckii",
-        "genus_species": "Espeletia funckii Sch.Bip. ex Wedd.",
-        "conservation_status": "Endangered",
-        "plant_type": "Subshrub or shrub",
-        "country_of_origin": "Colombia (Norte de Santander, Santander)",
-    },
-]
-
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Plant
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
 def about(request):
     return render(request, 'about.html')
 
 def plants_index(request):
+    plants = Plant.objects.all()
     return render(request, 'plants/index.html', {'plants': plants})
+
+def plants_detail(request, plant_id ):
+  plant = Plant.objects.get(id=plant_id)
+  return render(request, 'plants/detail.html', {'plant': plant})
+
+class PlantCreate(CreateView):
+  model = Plant
+  fields = '__all__'
+
+class PlantUpdate(UpdateView):
+  model = Plant
+  fields = '__all__'
+
+class PlantDelete(DeleteView):
+  model = Plant
+  success_url = '/plants'
